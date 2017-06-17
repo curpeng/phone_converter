e1cd5fd19eb7e0abc17d7bd0ee5401926e962268
Dolhishev Viktor <v.dolgishev@gmail.com>
2017-06-17 14:13:01 +0300

    adds validation, dictionary load and MAP structure

--- lib/phone_converter.rb
+++ lib/phone_converter.rb
@@ -1,12 +1,45 @@
 # frozen_string_literal: true
 
 require_relative 'errors'
+require 'pry'
 
 module PhoneConverter
+  NUMBER_LENGTH = 10
+
+  MAP = {
+    '2' => %w[a b c],
+    '3' => %w[d e f],
+    '4' => %w[g h i],
+    '5' => %w[j k l],
+    '6' => %w[m n o],
+    '7' => %w[p q r s],
+    '8' => %w[t u v],
+    '9' => %w[w x y z]
+  }.freeze
+
+  @dictionary = File.readlines(File.dirname(__FILE__) + '/dictionary.txt').map(&:strip)
+
   class << self
-    def convert(_number)
-      sleep 1
-      []
+    def convert(number)
+      number = number.to_s
+      validate(number)
+
+      dictionary = @dictionary.dup
+
+      number.chars.each_with_index do |digit, index|
+        appropriate_chars = MAP[digit].map(&:upcase)
+        dictionary = dictionary.select { |word| appropriate_chars.include?(word[index]) }
+      end
+
+      dictionary
+    end
+
+    private
+
+    def validate(number)
+      raise InvalidNumberError, 'number should contain 10 digits' if number.size < NUMBER_LENGTH
+      raise InvalidNumberError, "number shouldn't contain digit '0'" if number.include?('0')
+      raise InvalidNumberError, "number shouldn't contain digit '1'" if number.include?('1')
     end
   end
 end
